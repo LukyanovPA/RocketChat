05309268b91ace32b95724db7943b0d399427687
lukyanovpavel <pavel.lukyanovv@gmail.com>
2022-08-06 02:26:44 +0300

    refactor: changed Hilt to Dagger

--- app/src/main/java/com/pavellukyanov/rocketchat/presentation/MainViewModel.kt
+++ app/src/main/java/com/pavellukyanov/rocketchat/presentation/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.pavellukyanov.rocketchat.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.pavellukyanov.rocketchat.data.auth.AuthFirebase
 import com.pavellukyanov.rocketchat.domain.usecase.auth.IsAuthorized
 import com.pavellukyanov.rocketchat.presentation.base.BaseViewModel
 import com.pavellukyanov.rocketchat.utils.Constants.EMPTY_STRING
@@ -12,14 +13,17 @@ import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     navigator: MainNavigator,
-    private val isAuthorized: IsAuthorized
+    private val isAuthorized: IsAuthorized,
+    //TODO temp
+    private val authFirebase: AuthFirebase
 ) : BaseViewModel<MainNavigator>(navigator) {
     private val nickname = MutableStateFlow(EMPTY_STRING)
 
     fun checkAuth() = launchIO {
         isAuthorized().collect { state ->
             if (state) {
-                nickname.emit("Логин заебиська!")
+                //TODO temp
+                nickname.emit(authFirebase().currentUser?.displayName!!)
             } else {
                 launchUI { navigator.forwardToSignIn() }
             }
