46e62168fd95000dd12e8826c5aac420969b5c51
lukyanovpavel <pavel.lukyanovv@gmail.com>
2022-10-17 14:50:44 +0300

    refactor: messages date/time logic

--- app/src/main/java/com/pavellukyanov/rocketchat/utils/DateUtil.kt
+++ app/src/main/java/com/pavellukyanov/rocketchat/utils/DateUtil.kt
@@ -5,28 +5,30 @@ import com.pavellukyanov.rocketchat.utils.Constants.INT_ZERO
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 object DateUtil {
     private val dateFormatter = DateTimeFormatter.ofPattern("d.MM.yyyy")
     private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
+    private fun longToInstant(dateLong: Long): ZonedDateTime =
+        Instant.ofEpochMilli(dateLong).atZone(ZoneId.systemDefault())
+
     fun localDateToStringDate(date: LocalDate): String =
         date.format(dateFormatter)
 
-    fun localDateToStringTime(date: Long): String {
-        return Instant.ofEpochMilli(date).atZone(ZoneId.systemDefault()).toLocalTime().format(timeFormatter)
-    }
+    fun localDateToStringTime(date: Long): String =
+        longToInstant(date).toLocalTime().format(timeFormatter)
 
     fun longToLocalDate(dateLong: Long): LocalDate =
-        Instant.ofEpochMilli(dateLong).atZone(ZoneId.systemDefault()).toLocalDate()
+        longToInstant(dateLong).toLocalDate()
 
     fun longToDateStringDate(dateLong: Long): String =
         localDateToStringDate(longToLocalDate(dateLong))
 
     fun dateCompareWithToday(localDate: LocalDate): String {
         val today = LocalDate.now()
-
 
         return if (localDate.year != today.year || localDate.monthValue != today.monthValue) {
             localDateToStringDate(localDate)
